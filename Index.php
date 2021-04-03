4cfccace9eb77b8aaa0a9390eec08b248b95a905
Jaidubey25 <72821500+Jaidubey25@users.noreply.github.com>
2021-04-03 19:52:44 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,6 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Refresh" content="0; url=Index.php">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Monoton&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Grenze+Gotisch:wght@900&display=swap" rel="stylesheet">
